221a4b436051fce2c7b1f05f2fa9954d990d0d83
Cristian <cristianvillalobos666@gmail.com>
2024-06-30 22:44:44 -0400

    first commit1

---
+++ src/data/products.js
@@ -0,0 +1,28 @@
+// src/data/products.js
+export const products = [
+    {
+        id: "product1",
+        title: "Nike Air MX Super 2500 - Red",
+        description: "Descripción del producto",
+        imgSrc: "/product1.jpg",
+        price: "6.990",
+        oldPrice: "10.990"
+    },
+    {
+        id: "product2",
+        title: "Nike Air MX Super 2500 - Blue",
+        description: "Descripción del producto",
+        imgSrc: "/product1.jpg",
+        price: "8.990",
+        oldPrice: "14.990"
+    },
+    {
+        id: "product2",
+        title: "Nike Air MX Super 2500 - Blue",
+        description: "Descripción del producto",
+        imgSrc: "/product1.jpg",
+        price: "8.990",
+        oldPrice: "14.990"
+    },
+    // Añadir más productos según sea necesario
+];
